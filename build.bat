db648ec38feba7a8a0ede7257e39762fbbbac9b6
Gopi <gopiyadav4788@gmail.com>
2026-01-05 10:59:29 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 cl /EHsc ^
-/IG:\instantclient_23_9\sdk\include ^
+/ID:\instantclient_23_9\sdk\include ^
 main.cpp DBConnection.cpp Login.cpp Admin.cpp Instructor.cpp Hod.cpp Student.cpp ^
 /link ^
-/LIBPATH:G:\instantclient_23_9\sdk\lib\msvc ^
+/LIBPATH:D:\instantclient_23_9\sdk\lib\msvc ^
 oci.lib oraocci23.lib
 pause
+
